11bd6c856870971868524a31e3cbc9b3a6256459
Agustin Gaitan <gosagustingaitan@gmail.com>
2022-05-12 23:48:29 -0300

    REDUCERS

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterApp } from './components/01-useState/CounterApp';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-import { FocusScreen } from './components/04-useRef/FocusScreen';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-import { Layout } from './components/05-useLayoutEffect/Layout';
-import { CallbackHook } from './components/06-memos/CallbackHook';
-import { MemoHook } from './components/06-memos/MemoHook';
-import { Memorize } from './components/06-memos/Memorize';
-import { Padre } from './components/07-tarea-memo/Padre';
-import { HookApp } from './HookApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+// import { MemoHook } from './components/06-memos/MemoHook';
+// import { Memorize } from './components/06-memos/Memorize';
+// import { Padre } from './components/07-tarea-memo/Padre';
+// import { HookApp } from './HookApp';
+import { TodoApp } from './components/08-useReducer/TodoApp';
 import './index.css';
 
 
@@ -31,10 +32,9 @@ root.render(
   // <Memorize />
   // <MemoHook />
   // <CallbackHook/>
-  <Padre/>
+  // <Padre/>
+    <TodoApp />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+
+
