1577df5be046bb20805f8e958658d80a3c4c08d6
Agustin Gaitan <gosagustingaitan@gmail.com>
2022-06-03 02:55:57 -0300

    funciona reducer

--- 08-journal-app/src/reducers/authReducer.js
+++ 08-journal-app/src/reducers/authReducer.js
@@ -1,18 +1,73 @@
+import { createAction, createReducer, createSlice } from '@reduxjs/toolkit';
 import { types } from '../types/types';
 
 
-export const authReducer = (state = {}, action) =>{
+// export const authReducer = (state = {}, action) =>{
 
-    switch(action.type){
-        case types.login:
-            return{
-                uid: action.payload.uid,
-                name:action.payload.displayName
+//     switch(action.type){
+//         case types.login:
+//             return{
+//                 uid: action.payload.uid,
+//                 name:action.payload.displayName
 
-            }
-        case types.logout:
-            return {}
-        default:
-            return state;
-    }
-}
+//             }
+//         case types.logout:
+//             return {}
+//         default:
+//             return state;
+//     }
+// }
+
+const login = createAction(types.login);
+const logout = createAction(types.logout);
+const initialState = {uid:123123123};
+
+export const authReducer = createReducer(initialState, (builder) =>{
+
+    builder
+    .addCase(login, (state = {}, action) =>{
+        return{
+            uid: action.payload.uid,
+            name:action.payload.displayName
+
+        }
+    })
+    .addCase(logout, (state = {},action)=>{
+        return {}
+    })
+    .addDefaultCase((state = {},action)=> {});
+
+});
+
+
+// export const { authReducer } = reducer.actions;
+
+// export default reducer.reducer;
+
+
+// export const reducer = createSlice({
+//     name: 'auth',
+//     initialState: {},
+//     reducers:{
+//         authReducer: (state = {}, action) => {
+
+//                 switch(action.type){
+//                     case types.login:
+//                         return{
+//                             uid: action.payload.uid,
+//                             name:action.payload.displayName
+
+//                         }
+//                     case types.logout:
+//                         return {}
+//                     default:
+//                         return state;
+//                 }
+
+//         },
+//     },
+// });
+
+// export const { authReducer } = reducer.actions;
+
+// export default reducer.reducer;
--- 08-journal-app/src/store/store.js
+++ 08-journal-app/src/store/store.js
@@ -1,11 +1,14 @@
 import {combineReducers } from 'redux';
-import { authReducer } from '../reducers/authReducer';
+// import {authReducer}  from '../reducers/authReducer';
+import {authReducer}  from '../reducers/authReducer';
 import { configureStore } from '@reduxjs/toolkit'
 
 
-const reducers = combineReducers({
-    auth: authReducer
+
+
+
+export const store = configureStore({
+    reducer: {
+        auth:authReducer
+    }
 });
-
-
-export const store = configureStore(reducers);
