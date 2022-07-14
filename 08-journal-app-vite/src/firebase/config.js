02b47fa2a913f28d42917dbd7450e4e57fdb69a8
Agustin Gaitan <gosagustingaitan@gmail.com>
2022-07-14 19:30:36 -0300

    firebase agregado

---
+++ 08-journal-app-vite/src/firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore}  from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJgxrUtgGeFl1IL196kWCTjh4piFaujsY",
+  authDomain: "testfirebase-6cfa8.firebaseapp.com",
+  projectId: "testfirebase-6cfa8",
+  storageBucket: "testfirebase-6cfa8.appspot.com",
+  messagingSenderId: "842450847256",
+  appId: "1:842450847256:web:67d40a60041ccdd38ae56d"
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+
+export const FirebaseAuth = getAuth(FirebaseApp);
+
+export const firebaseDB = getFirestore(FirebaseApp);
--- 08-journal-app-vite/src/main.jsx
+++ 08-journal-app-vite/src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { JournalApp } from './JournalApp';
 import {BrowserRouter} from 'react-router-dom';
+import {store} from './store';
 import './styles.css'
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
     <JournalApp />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
