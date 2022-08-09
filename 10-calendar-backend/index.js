bf688c37a8f871c1e95ed549f3ee0f47e98f4e86
Agustin Gaitan <gosagustingaitan@gmail.com>
2022-08-08 23:19:22 -0300

    empezando backend

---
+++ 10-calendar-backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+//Crear el servidor de express
+const app = express();
+
+
+//Rutas
+app.get('/', (request, response)=>{
+
+  
+
+    response.json({
+        ok:true
+    });
+
+});
+
+
+
+//Escuchar peticiones
+
+app.listen(4000, () =>{
+    console.log(`Servidor corriendo en puerto ${4000}`);
+});
