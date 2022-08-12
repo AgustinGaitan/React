e4ea53679214147cd886113348e924ab56a385e2
Agustin Gaitan <gosagustingaitan@gmail.com>
2022-08-12 20:40:05 -0300

    frontend

--- 10-calendar-frontend/src/auth/pages/LoginPage.jsx
+++ 10-calendar-frontend/src/auth/pages/LoginPage.jsx
@@ -1,78 +1,152 @@
+import { useEffect } from 'react';
+import Swal from 'sweetalert2';
+import { useAuthStore, useForm } from '../../hooks';
 import './LoginPage.css';
 
+const loginFormFields = {
+    loginEmail: '',
+    loginPassword: ''
+}
+
+const registerFormFields = {
+    registerEmail:'',
+    registerName:'',
+    registerPassword:'',
+    registerPassword2:'',
+}
+
+
+
 export const LoginPage = () => {
-  return (
-      <div className="container login-container">
-          <div className="row">
-              <div className="col-md-6 login-form-1">
-                  <h3>Ingreso</h3>
-                  <form>
-                      <div className="form-group mb-2">
-                          <input 
-                              type="text"
-                              className="form-control"
-                              placeholder="Correo"
-                          />
-                      </div>
-                      <div className="form-group mb-2">
-                          <input
-                              type="password"
-                              className="form-control"
-                              placeholder="Contraseña"
-                          />
-                      </div>
-                      <div className="d-grid gap-2">
-                          <input 
-                              type="submit"
-                              className="btnSubmit"
-                              value="Login" 
-                          />
-                      </div>
-                  </form>
-              </div>
 
-              <div className="col-md-6 login-form-2">
-                  <h3>Registro</h3>
-                  <form>
-                      <div className="form-group mb-2">
-                          <input
-                              type="text"
-                              className="form-control"
-                              placeholder="Nombre"
-                          />
-                      </div>
-                      <div className="form-group mb-2">
-                          <input
-                              type="email"
-                              className="form-control"
-                              placeholder="Correo"
-                          />
-                      </div>
-                      <div className="form-group mb-2">
-                          <input
-                              type="password"
-                              className="form-control"
-                              placeholder="Contraseña" 
-                          />
-                      </div>
+    const {loginEmail, loginPassword, onInputChange : onLoginInputChange} = useForm(loginFormFields);
+    const {registerEmail, registerName,registerPassword, registerPassword2, onInputChange : onRegisterInputChange} = useForm(registerFormFields);
 
-                      <div className="form-group mb-2">
-                          <input
-                              type="password"
-                              className="form-control"
-                              placeholder="Repita la contraseña" 
-                          />
-                      </div>
+    const {startLogin, startRegister, errorMessage} = useAuthStore();
 
-                      <div className="d-grid gap-2">
-                          <input 
-                              type="submit" 
-                              className="btnSubmit" 
-                              value="Crear cuenta" />
-                      </div>
-                  </form>
-              </div>
-          </div>
-      </div>
-  )
+    useEffect(() => {
+      
+        if(errorMessage !== undefined){
+            Swal.fire({
+                title:'Error en la autenticacion',
+                text:errorMessage,
+                icon:'error'
+            });
+        }
+
+    }, [errorMessage])
+    
+
+    const loginSubmit = (event) =>{
+        event.preventDefault();
+        startLogin({email: loginEmail, password: loginPassword});
+    }
+
+    const registerSubmit = (event) =>{
+        event.preventDefault();
+
+        if(registerPassword !== registerPassword2){
+            Swal.fire({
+                title:'Error en el registro',
+                text:'Las contraseñas no son iguales',
+                icon:'error'
+            });
+
+            return;
+        }
+        startRegister({registerName, registerEmail, registerPassword})
+    }
+
+    return (
+        <div className="container login-container">
+            <div className="row">
+                <div className="col-md-6 login-form-1">
+                    <h3>Ingreso</h3>
+                    <form onSubmit={loginSubmit}>
+                        <div className="form-group mb-2">
+                            <input 
+                                type="text"
+                                className="form-control"
+                                placeholder="Correo"
+                                name="loginEmail"
+                                value={loginEmail}
+                                onChange={onLoginInputChange}
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Contraseña"
+                                name="loginPassword"
+                                value={loginPassword}
+                                onChange={onLoginInputChange}
+                            />
+                        </div>
+                        <div className="d-grid gap-2">
+                            <input 
+                                type="submit"
+                                className="btnSubmit"
+                                value="Login" 
+                            />
+                        </div>
+                    </form>
+                </div>
+
+                <div className="col-md-6 login-form-2">
+                    <h3>Registro</h3>
+                    <form onSubmit={registerSubmit}>
+                        <div className="form-group mb-2">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Nombre"
+                                name="registerName"
+                                value={registerName}
+                                onChange={onRegisterInputChange}
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Correo"
+                                name="registerEmail"
+                                value={registerEmail}
+                                onChange={onRegisterInputChange}
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Contraseña" 
+                                name="registerPassword"
+                                value={registerPassword}
+                                onChange={onRegisterInputChange}
+                            />
+                        </div>
+
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Repita la contraseña" 
+                                name="registerPassword2"
+                                value={registerPassword2}
+                                onChange={onRegisterInputChange}
+                            />
+                        </div>
+
+                        <div className="d-grid gap-2">
+                            <input 
+                                type="submit" 
+                                className="btnSubmit" 
+                                value="Crear cuenta" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
--- 10-calendar-frontend/src/router/AppRouter.jsx
+++ 10-calendar-frontend/src/router/AppRouter.jsx
@@ -1,13 +1,27 @@
+import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import {LoginPage} from "../auth";
 import { CalendarPage} from '../calendar';
+import { useAuthStore } from "../hooks";
 
 
 
 export const AppRouter = () => {
-    
-  const authStatus = 'authenticated';
- 
+  
+  const {status, checkAuthToken} = useAuthStore();
+  //const authStatus = 'not-authenticated';
+  
+  useEffect(() => {
+    checkAuthToken();
+  },[])
+  
+
+  if(status === 'checking'){
+    return(
+      <h3>Cargando...</h3>
+    )
+  }
+
 
   return (
     
@@ -15,15 +29,24 @@ export const AppRouter = () => {
 
         {
 
-          (authStatus==='not-authenticated')
+          (status==='not-authenticated')
           ?
-          <Route path="/auth/*" element={<LoginPage/>}/>
+            (
+              <>
+                <Route path="/auth/*" element={<LoginPage/>}/>
+                <Route path="/*" element={<Navigate to="/auth/login/"/>}/>
+              </>
+            )
           :
-          <Route path="/*" element={<CalendarPage/>}/>
-
+          (
+            <>  
+              <Route path="/" element={<CalendarPage/>}/>
+              <Route path="/*" element={<Navigate to='/' />} />
+            </>
+          )
+        
         } 
 
-        <Route path="/*" element={<Navigate to="/auth/login/"/>}/>
     </Routes>
   )
   
